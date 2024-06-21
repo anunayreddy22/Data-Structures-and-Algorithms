49deafa20dedd90df205cfbcaa4e8b91e94845b5
anunayreddy22 <156383908+anunayreddy22@users.noreply.github.com>
2024-06-21 10:25:13 -0500

    Create Selection sort.java

---
+++ Sorting Algorithms/Selection sort.java
@@ -0,0 +1,30 @@
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int[] arr = new int[n];
+		for(int i=0;i<n;i++){
+		    arr[i] = sc.nextInt();
+		}
+		for (int i = 0; i < n - 1; i++) { 
+            // Find the minimum element in unsorted array 
+            int min_idx = i; 
+            for (int j = i + 1; j < n; j++) { 
+                if (arr[j] < arr[min_idx]) 
+                    min_idx = j; 
+            } 
+  
+            // Swap the found minimum element with the first 
+            // element 
+            int temp = arr[min_idx]; 
+            arr[min_idx] = arr[i]; 
+            arr[i] = temp; 
+        }
+        //print the sorted array finally
+        for(int i=0;i<n;i++){
+            System.out.print(arr[i]+" ");
+        }
+	}
+}
