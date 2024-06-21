f09e0a0e7bac0d69ac8b0f996d13e8b57ca2cd2a
anunayreddy22 <156383908+anunayreddy22@users.noreply.github.com>
2024-06-21 10:05:43 -0500

    Update Bubble sort.java

--- Sorting Algorithms/Bubble sort.java
+++ Sorting Algorithms/Bubble sort.java
@@ -1,1 +1,27 @@
-
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    // Taking user input for array
+		int n = sc.nextInt();
+		int[] arr = new int[n];
+		for(int i=0;i<n;i++){
+		    arr[i] = sc.nextInt();
+		}
+    // Bubble sort logic
+		for (int i = 0; i < n - 1; i++){
+            for (int j = 0; j < n - i - 1; j++){
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+		}
+    // printing the sorted array
+    for(int i=0;i<n;i++){
+            System.out.print(arr[i]+" ");
+      }
+  }
+}
