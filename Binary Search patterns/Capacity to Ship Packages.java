5ae061e1588d31b8bc661c0ba2d5d060bac0c3f1
anunayreddy22 <156383908+anunayreddy22@users.noreply.github.com>
2024-07-02 11:07:09 -0500

    Update Capacity to Ship Packages.java

--- Binary Search patterns/Capacity to Ship Packages.java
+++ Binary Search patterns/Capacity to Ship Packages.java
@@ -1,1 +1,31 @@
 // Leetcode 1011
+class Solution {
+    private int possible (int capacity, int[] weights) {
+        int sum = 0, days = 1;
+        for (int w : weights) {
+            sum += w;
+            if (sum > capacity) {
+                days++;
+                sum = w;
+            }
+        }
+        return days;
+    }
+	
+    public int shipWithinDays(int[] weights, int D) {
+        int min = 0, max = 0;
+        for (int w : weights) {
+            max += w;
+            min = Math.max(w, min);
+        }
+        while (min <= max) {
+            int mid = min + (max - min)/2;
+            if (possible(mid, weights) > D) {
+                min = mid + 1;
+            } else {
+                max = mid - 1;
+            }
+        }
+        return min;
+    }
+}
