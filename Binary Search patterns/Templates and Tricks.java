f04106791e5e750ce0dc37e69e80572a770b9aa2
anunayreddy22 <156383908+anunayreddy22@users.noreply.github.com>
2024-07-02 10:01:00 -0500

    Update Templates and Tricks.java

--- Binary Search patterns/Templates and Tricks.java
+++ Binary Search patterns/Templates and Tricks.java
@@ -1,2 +1,23 @@
+here is general approach of binary search
 
-n
+ int BinarySearchTemplate(int[] v, int cond) 
+{
+        int low= 0 , high=somehighvalue,ans=choose value according to question;
+        while(high>=low)
+        {
+            int mid=(low+high)/2;
+            if(findisBoundary(mid, h,cond))
+            {     
+                Two case
+                 //1. shrink high=mid-1 or mid+1 and calucate answer
+                 //2. shring low=mid+1 or mid+1 and calculate answer 
+            }
+            else
+            {
+                Two case
+               //1. shrink high=mid-1 or mid+1 and calucate answer
+                 //2. shring low=mid+1 or mid+1 and calculate answer 
+            }
+        }
+        return ans ;
+    }
